d92e34b2426a86ab28361e06276930189d5775f5
Donnie West <igneo676@gmail.com>
2016-01-14 20:54:02 -0600

    Switch to formspree

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,14 +18,15 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "https://formkeep.com/f/a363bbabde41",
-                type: "POST",
+                url: "//formspree.io/igneo676@gmail.com",
+                method: "POST",
                 data: {
                     name: name,
                     phone: phone,
                     email: email,
                     message: message
                 },
+                dataType: "json",
                 cache: false,
                 success: function() {
                     // Success message
